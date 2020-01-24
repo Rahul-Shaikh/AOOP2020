669ba9edc6fb5401bba4adf01fb8d58603fb6346
Rahul Shaikh <rahulshaikh5720.ac@gmail.com>
2020-01-25 02:00:33 +0530

    General customer complete

--- Assignment-2/Banking/Customer.java
+++ Assignment-2/Banking/Customer.java
@@ -1,24 +1,51 @@
-class Customer {
+import java.util.Scanner;
+
+abstract class Customer {
 	static long lastid = 19001100;
 	long cid;
 	String name;
 	double currLoan;
 	String phNo;
-	static protected Scanner psc = Scanner(System.in);
-	 
+	static protected Scanner psc = new Scanner(System.in);
+
 	Customer() {
 		cid = (++lastid);
 	}
-	
+
 	void setValues() {
 		System.out.print("ENTER NAME: ");
 		name = psc.nextLine();
 		System.out.print("ENTER PHONE NUMBER: ");
 		phNo = psc.nextLine();
 	}
-	
-	void showValues() {
-		
+
+	void showValues() { 
+		System.out.println("\n--------------------------------------------");
+		System.out.println("ID: " + cid);
+		System.out.println("NAME: " + name);
+		System.out.println("PHONE NUMBER: " + phNo);
+		System.out.println("LOAN: " + currLoan);
+		System.out.println("--------------------------------------------\n");
 	}
-	// abstract boolean isExceeded();
+
+	void payLoan() {
+		while (true) {
+			System.out.print("ENTER THE AMOUNT YOU WANNA PAY: ");
+			double temp = psc.nextDouble();
+			if (currLoan - temp < 0) {
+				System.out.println("YOU ARE PAYING MORE THAN THE LOAN. PLEASE CORRECT.\n");
+			}
+			else {
+				currLoan -= temp;
+				break;
+			}
+		}
+	}
+	abstract void requestLoan();
+
+	abstract boolean isExceeded(double temp);
+
+	abstract void showLimit();
+
+	abstract void loanLeft(double temp);
 }
--- Assignment-2/Banking/Main.java
+++ Assignment-2/Banking/Main.java
@@ -1,8 +1,15 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		GenCustomer c = new GenCustomer();
+		c.setValues();
+		c.showValues();
 		
+		c.requestLoan();
+		c.loanLeft(0);
+		c.showValues();
+		c.payLoan();
+		c.showValues();
 	}
 }
