fadef02334264f65ae3bd6080ed59e8e4bc06c9f
rahul5720 <rahulshaikh5720.ac@gmail.com>
2020-02-14 17:06:58 +0530

    Library BookList updated 2

---
+++ Assignment-2/Library/MemberList/Member.java
@@ -0,0 +1,18 @@
+public class Member {
+	String id;
+	String name;
+	int numIssued = 0;
+	static final int limit = 5;
+	class DOB {
+		int d;
+		int m;
+		int y;
+		
+		void getInput() {
+		}
+		
+	}
+	
+	DOB dob = new DOB();
+	
+}
